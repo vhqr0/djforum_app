036abde333d7949a03e79ed9237d2593591523d6
vhqr0 <zq_cmd@163.com>
2023-02-12 20:33:10 +0800

    clean commands

--- management/commands/djfclear.py
+++ management/commands/djfclear.py
@@ -1,18 +1,16 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
-from ...models import VerifyRecord, LikeTopic, LikeReply
+from ...models import LikeTopic, LikeReply
 
 from datetime import timedelta
 
 
 class Command(BaseCommand):
-    help = ('DJForum: clear the verify records and topic/reply likes'
-            ' long than 30 days.')
+    help = ('DJForum: clear topic/reply likes long than 30 days.')
 
     def handle(self, *args, **kwargs):
         dt = timezone.now() - timedelta(days=30)
-        VerifyRecord.objects.filter(time_sent__lt=dt).delete()
         LikeTopic.objects.filter(date_created__lt=dt).delete()
         LikeReply.objects.filter(date_created__lt=dt).delete()
         self.stdout.write(
